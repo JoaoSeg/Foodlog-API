ec783714cad280c76848563bbf2be203c343a997
JoaoSeg <joaosegolin@gmail.com>
2023-08-23 10:05:00 -0300

    Generate Test files for archive mvc

--- test/system/entries_test.rb
+++ test/system/entries_test.rb
@@ -2,17 +2,17 @@ require "application_system_test_case"
 
 class EntriesTest < ApplicationSystemTestCase
   setup do
-    @entry = entries(:one)
+    @entry = entries(:breakfast)
   end
 
   test "visiting the index" do
     visit entries_url
-    assert_selector "h1", text: "Entries"
+    assert_selector "p.title", text: "FoodLog – #{DateTime.now.strftime("%B %d, %Y")}" 
   end
 
   test "should create entry" do
     visit entries_url
-    click_on "New entry"
+    click_on "Create new entry"
 
     fill_in "Calories", with: @entry.calories
     fill_in "Carbohydrates", with: @entry.carbohydrates
@@ -22,12 +22,11 @@ class EntriesTest < ApplicationSystemTestCase
     click_on "Create Entry"
 
     assert_text "Entry was successfully created"
-    click_on "Back"
   end
 
   test "should update Entry" do
     visit entry_url(@entry)
-    click_on "Edit this entry", match: :first
+    click_on "Edit", match: :first
 
     fill_in "Calories", with: @entry.calories
     fill_in "Carbohydrates", with: @entry.carbohydrates
@@ -37,13 +36,12 @@ class EntriesTest < ApplicationSystemTestCase
     click_on "Update Entry"
 
     assert_text "Entry was successfully updated"
-    click_on "Back"
   end
 
   test "should destroy Entry" do
     visit entry_url(@entry)
-    click_on "Destroy this entry", match: :first
+    click_on "Delete", match: :first
 
-    assert_text "Entry was successfully destroyed"
+    asseert_selector "alert", text: "Are you sure?"
   end
 end
